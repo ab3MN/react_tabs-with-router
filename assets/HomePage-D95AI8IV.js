b6ec4498cbb65603858944604d93610ffc0c937c
ab3MN <ab3mn3@gmail.com>
2024-11-17 00:46:58 +0200

    Updates

---
+++ assets/HomePage-D95AI8IV.js
@@ -0,0 +1,1 @@
+import{j as e}from"./index-CxZ1ZuMv.js";const t=()=>e.jsx("h1",{className:"title",children:"Home page"});export{t as default};
---
+++ assets/NotFoundPage-C1hWwIOO.js
@@ -0,0 +1,1 @@
+import{j as t}from"./index-CxZ1ZuMv.js";const e=()=>t.jsx("h1",{className:"title",children:"Page not found"});export{e as default};
